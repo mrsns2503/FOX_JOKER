c274eaecf6adbf728fd3b967153c5692d8707b9e
mrsns2503 <121515204+mrsns2503@users.noreply.github.com>
2023-03-08 00:03:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mrsns2503/SNS_P_FILTER /SNS_P_FILTER
+  git clone https://github.com/mrsns2503/FOX_JOKER /FOX_JOKER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /SNS_P_FILTER
+  git clone $UPSTREAM_REPO /FOX_JOKER
 fi
-cd /SNS_P_FILTER
+cd /FOX_JOKER
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
